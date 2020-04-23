5a574dd29ea9942e6e1d87b306267089c7e8f21a
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2020-04-23 08:54:24 +0200

    Add basic auth

--- src/main.py
+++ src/main.py
@@ -1,11 +1,43 @@
-from fastapi import FastAPI, Request, HTTPException
+import secrets
+import uuid
+
+from fastapi import FastAPI, Request, HTTPException, Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security.base import SecurityBase
+from pydantic import BaseModel
+from starlette import status
+from starlette.responses import RedirectResponse
+
+from typing import Optional
+import base64
+# from passlib.context import CryptContext
+# from datetime import datetime, timedelta
+#
+# import jwt
+# from jwt import PyJWTError
+
 from pydantic import BaseModel
 
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2
+from fastapi.security.base import SecurityBase
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.utils import get_openapi
+
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.responses import RedirectResponse, Response, JSONResponse
+from starlette.requests import Request
+
 app = FastAPI()
+security = HTTPBasic()
 
 app.counter = 0
 app.next_patient_id = -1
 app.patients = {}
+app.sessions = {}
 
 
 class HelloNameResponse(BaseModel):
@@ -39,6 +71,34 @@ class PatientResponse(BaseModel):
     surename: str
 
 
+class BasicAuth(SecurityBase):
+    def __init__(self, scheme_name: str = None, auto_error: bool = True):
+        self.scheme_name = scheme_name or self.__class__.__name__
+        self.auto_error = auto_error
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "basic":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                )
+            else:
+                return None
+        return param
+
+
+basic_auth = BasicAuth(auto_error=False)
+fake_users_db = {'trudnY': 'PaC13Nt'}
+
+
+def authenticate_user(users_db, username, password):
+    if username in users_db and users_db[username] == password:
+        return username
+    return None
+
+
 @app.get('/')
 def hello_world():
     return {'message': 'Hello World during the coronavirus pandemic!'}
@@ -47,6 +107,31 @@ def hello_world():
 @app.get('/welcome')
 def welcome():
     return {'message': 'Welcome!'}
+
+
+@app.post('/login')
+def authenticate(auth: BasicAuth = Depends(basic_auth)):
+    if not auth:
+        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=403)
+        return response
+
+    try:
+        decoded = base64.b64decode(auth).decode("ascii")
+        username, _, password = decoded.partition(":")
+        user = authenticate_user(fake_users_db, username, password)
+        if user is None:
+            raise HTTPException(status_code=401, detail="Incorrect email or password")
+
+        session_id = uuid.uuid4()
+        app.sessions[session_id] = {'username': user}
+
+        response = RedirectResponse(url='/welcome', status_code=status.HTTP_302_FOUND)
+        response.set_cookie('SESSIONID', value=str(session_id), httponly=True, max_age=1800, expires=1800)
+        return response
+
+    except Exception:
+        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
+        return response
 
 
 @app.get('/hello/{name}', response_model=HelloNameResponse)
